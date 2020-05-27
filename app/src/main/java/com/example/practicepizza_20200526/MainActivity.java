a5076c872b908c8b1dbb91a9a6df666154406402
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-27 10:56:27 +0900

    피자가게 리스트를 arraylist에 추가  > 이미지 url로 입력

--- app/src/main/java/com/example/practicepizza_20200526/MainActivity.java
+++ app/src/main/java/com/example/practicepizza_20200526/MainActivity.java
@@ -6,9 +6,14 @@ import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 
 import com.example.practicepizza_20200526.databinding.ActivityMainBinding;
+import com.example.practicepizza_20200526.datas.PizzaStore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends BaseActivity {
     ActivityMainBinding binding;
+    List<PizzaStore> pizzaStoreList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +29,13 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+        addPizzaStores();
+    }
 
+    void addPizzaStores() {
+        pizzaStoreList.add(new PizzaStore("피자헛","02-4567-7890","https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"));
+        pizzaStoreList.add(new PizzaStore("파파존스","02-1234-5678","http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800"));
+        pizzaStoreList.add(new PizzaStore("미스터피자","02-0123-4567","https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200"));
+        pizzaStoreList.add(new PizzaStore("도미노피자","02-7894-5612","https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png"));
     }
 }
