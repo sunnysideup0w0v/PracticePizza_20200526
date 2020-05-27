daf5fab5c280152ab0cecc5c84f56a4675a52e54
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-27 13:15:31 +0900

    피자가게 전화 걸기 기능 추가  - TedPermission 활용

--- app/src/main/java/com/example/practicepizza_20200526/PizzaStoreDetailActivity.java
+++ app/src/main/java/com/example/practicepizza_20200526/PizzaStoreDetailActivity.java
@@ -4,22 +4,30 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.practicepizza_20200526.databinding.ActivityDetailPizzaStoreBinding;
 import com.example.practicepizza_20200526.datas.PizzaStore;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class PizzaStoreDetailActivity extends BaseActivity {
     ActivityDetailPizzaStoreBinding binding;
     PizzaStore mStore;
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this,R.layout.activity_detail_pizza_store);
         setupEvents();
         setValues();
@@ -27,7 +35,29 @@ public class PizzaStoreDetailActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        binding.callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PermissionListener permissionListener = new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+                        Uri myUri = Uri.parse(String.format("tel:%s",mStore.getPhoneNum()));
+                        Intent myIntent = new Intent(Intent.ACTION_CALL,myUri);
+                        startActivity(myIntent);
+                    }
 
+                    @Override
+                    public void onPermissionDenied(List<String> deniedPermissions) {
+                        Toast.makeText(mContext, "통화 권한을 허가해줘야 통화가 가능합니다.", Toast.LENGTH_SHORT).show();
+                    }
+                };
+                TedPermission.with(mContext)
+                        .setPermissionListener(permissionListener)
+                        .setDeniedMessage("설정에서 권한 승인이 필요합니다.")
+                        .setPermissions(Manifest.permission.CALL_PHONE)
+                        .check();
+            }
+        });
     }
 
     @Override
